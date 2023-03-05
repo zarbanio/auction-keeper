56932f57f15387988b46f353f88db6d0cc3228e7
xana-rahmani <xanarahmani@gmail.com>
2023-03-05 13:32:11 +0330

    Remove clipperDog.go

--- store/migrations/06-create-clipper-dogs.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE if not exists clipper_dogs
-(
-    id              SERIAL PRIMARY KEY,
-    ilk             TEXT,
-	urn             TEXT,
-    tx_id           INTEGER UNIQUE REFERENCES transactions (id)
-);
-
-CREATE INDEX if not exists clipper_dog_urn_index on clipper_dogs(urn);
