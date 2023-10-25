7cc3b6c2c6445bd6e75a13c993d973c9ba2d4016
Arash Alaei <arashalaei@aut.ac.ir>
2023-10-25 14:36:53 +0330

    fix: dog bark service

--- services/processor/dog/bark.go
+++ services/processor/dog/bark.go
@@ -3,36 +3,35 @@ package processor
 import (
 	"context"
 	"log"
-	"math/big"
+	"time"
 
-	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/zarbanio/auction-keeper/bindings/zarban/dog"
 	"github.com/zarbanio/auction-keeper/bindings/zarban/osm"
 	"github.com/zarbanio/auction-keeper/bindings/zarban/spot"
-	"github.com/zarbanio/auction-keeper/domain/entities"
 	inputMethods "github.com/zarbanio/auction-keeper/domain/entities/inputMethods"
-	"github.com/zarbanio/auction-keeper/domain/math"
 	"github.com/zarbanio/auction-keeper/services/loaders"
 	"github.com/zarbanio/auction-keeper/services/transaction"
 	"github.com/zarbanio/auction-keeper/store"
+	"github.com/zarbanio/auction-keeper/x/eth"
 )
 
 type dogBarkService struct {
-	eth         *ethclient.Client
-	store       store.IStore
-	dog         *dog.Dog
-	spot        *spot.Spot
-	vaultLoader *loaders.VaultLoader
-	vatLoader   *loaders.VatLoader
-	keeper      *transaction.Sender
-	opts        *bind.TransactOpts
+	eth           *ethclient.Client
+	blockInterval time.Duration
+	store         store.IStore
+	dog           *dog.Dog
+	spot          *spot.Spot
+	vaultLoader   *loaders.VaultLoader
+	vatLoader     *loaders.VatLoader
+	keeper        *transaction.Sender
 }
 
-func NewDogService(
+func NewDogBarkService(
 	eth *ethclient.Client,
+	blockInterval time.Duration,
 	store store.IStore,
 	dogAddr common.Address,
 	spotAddr common.Address,
@@ -50,20 +49,15 @@ func NewDogService(
 		log.Fatal(err)
 	}
 
-	opts, err := keeper.GetOpts()
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	return &dogBarkService{
-		eth:         eth,
-		dog:         d,
-		spot:        s,
-		store:       store,
-		vaultLoader: vaultLoader,
-		vatLoader:   vatLoader,
-		keeper:      keeper,
-		opts:        opts,
+		eth:           eth,
+		blockInterval: blockInterval,
+		dog:           d,
+		spot:          s,
+		store:         store,
+		vaultLoader:   vaultLoader,
+		vatLoader:     vatLoader,
+		keeper:        keeper,
 	}
 }
 
@@ -74,19 +68,6 @@ func (s *dogBarkService) Start(ctx context.Context) error {
 	// Fetch all vaults
 	vaults, err := s.vaultLoader.FetchAllVaults(ctx)
 
-	if err != nil {
-		return err
-	}
-	// obtain params of isVaultUnsafe func PHASE 1
-
-	// 1) dog Hole
-	dogHole, err := s.dog.Hole(&bind.CallOpts{Context: ctx})
-	if err != nil {
-		return err
-	}
-
-	// 2) dog Dirt
-	dogDirt, err := s.dog.Dirt(&bind.CallOpts{Context: ctx})
 	if err != nil {
 		return err
 	}
@@ -109,55 +90,23 @@ func (s *dogBarkService) Start(ctx context.Context) error {
 				return err
 			}
 
-			osm, err := osm.NewOsm(ilk.Pip, s.eth)
+			// create a new Oracle Security Manager(OSM) and call the Poke method
+			err := s.newOsmAndPoke(ctx, ilk.Pip)
 			if err != nil {
-				log.Fatal(err)
+				return nil
 			}
-
-			tx, err := osm.Poke(s.opts)
+			// call the Poke method from spotter
+			err = s.spotterPoke(ctx, ilkName)
 			if err != nil {
 				return nil
 			}
 
-			log.Println("\tOSM Transaction: ", tx.Hash().Hex())
-
-			tx, err = s.spot.Poke(s.opts, ilkName)
-
-			if err != nil {
-				return nil
+			log.Println("Dog Bark is running")
+			bark := &inputMethods.DogBark{
+				Ilk: ilkName,
+				Urn: vault.Urn,
 			}
-
-			log.Println("\tSpotter Transaction: ", tx.Hash().Hex())
-
-			// obtain params of isVaultUnsafe func PHASE 2
-			v, err := s.vaultLoader.GetVaultByIlkUrn(ctx, ilkName, vault.Urn)
-			if err != nil {
-				return nil
-			}
-			// 1) urn
-			urn := entities.Urn{
-				Ink: v.Urn.Ink,
-				Art: v.Urn.Art,
-			}
-			// 2) vat ilk
-			vatIlk, err := s.vatLoader.GetIlkById(ctx, ilkName)
-			if err != nil {
-				return nil
-			}
-
-			// 3) dog chop
-			dogChop, err := s.dog.Chop(&bind.CallOpts{Context: ctx}, ilkName)
-			if err != nil {
-				return err
-			}
-			if s.isVaultUnSafe(urn, *vatIlk, dogHole, dogDirt, ilk.Hole.BigInt(), ilk.Dirt.BigInt(), dogChop) {
-				log.Println("Dog Bark is running")
-				bark := &inputMethods.DogBark{
-					Ilk: ilkName,
-					Urn: vault.Urn,
-				}
-				go s.Run(bark) // Run the service logic in a goroutine
-			}
+			s.Run(bark)
 		}
 
 	}
@@ -172,7 +121,12 @@ func (s *dogBarkService) Stop() {
 // Run the service logic
 func (s *dogBarkService) Run(bark *inputMethods.DogBark) (*types.Transaction, error) {
 
-	tx, err := s.dog.Bark(s.opts, bark.Ilk, bark.Urn, s.keeper.GetAddress())
+	opts, err := s.keeper.GetOpts()
+	if err != nil {
+		return nil, err
+	}
+
+	tx, err := s.dog.Bark(opts, bark.Ilk, bark.Urn, s.keeper.GetAddress())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -182,57 +136,103 @@ func (s *dogBarkService) Run(bark *inputMethods.DogBark) (*types.Transaction, er
 	return tx, nil
 }
 
-func (s *dogBarkService) isVaultUnSafe(urn entities.Urn, vatIlk entities.VatIlk, dogHole, dogDirt, ilkHole, ilkDirt, chop *big.Int) bool {
-	/*
-			Collateral value should be less than the product of our stablecoin debt and the debt multiplier
-		    require(spot > 0 && mul(ink, spot) < mul(art, rate), "Dog/not-unsafe");
-	*/
-	if vatIlk.Spot.Cmp(big.NewInt(0)) <= 0 {
-		return false
-	}
+func (s *dogBarkService) waitForReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
+	ticker := time.NewTicker(s.blockInterval)
+	defer ticker.Stop()
 
-	safe := new(big.Int).Mul(urn.Ink, vatIlk.Spot).Cmp(new(big.Int).Mul(urn.Art, vatIlk.Rate)) >= 0
-	if safe {
-		return false
-	}
-
-	/*
-	   Ensure there's room in the dog.hole
-	   require(Hole > Dirt && milk.hole > milk.dirt, "Dog/liquidation-limit-hit");
-	*/
-	if dogHole.Cmp(dogDirt) <= 0 {
-		return false
-	}
-	dogRoom := new(big.Int).Sub(dogHole, dogDirt)
-
-	/*
-	   Ensure there's room in the collateral-specific hole
-	   require(Hole > Dirt && milk.hole > milk.dirt, "Dog/liquidation-limit-hit");
-	*/
-	if ilkHole.Cmp(ilkDirt) <= 0 {
-		return false
-	}
-	ilkRoom := new(big.Int).Sub(ilkHole, ilkDirt)
-
-	/*
-	   Prevent dusty partial liquidation
-	*/
-	room := math.BigMin(dogRoom, ilkRoom)
-	wadRoom := new(big.Int).Mul(room, math.Wad)
-	dart := math.BigMin(urn.Art, new(big.Int).Div(new(big.Int).Div(wadRoom, vatIlk.Rate), chop))
-	if urn.Art.Cmp(dart) > 0 {
-		if new(big.Int).Mul(new(big.Int).Sub(urn.Art, dart), vatIlk.Rate).Cmp(vatIlk.Dust) < 0 {
-			dart = urn.Art
-		} else if new(big.Int).Mul(dart, vatIlk.Rate).Cmp(vatIlk.Dust) < 0 {
-			// require(mul(dart, rate) >= dust, "Dog/dusty-auction-from-partial-liquidation");
-			return false
+	for {
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		case <-ticker.C:
+			receipt, err := s.eth.TransactionReceipt(ctx, txHash)
+			if err == nil {
+				return receipt, nil
+			}
 		}
 	}
+}
 
-	dink := new(big.Int).Div(new(big.Int).Mul(urn.Ink, dart), urn.Art)
-	if dink.Cmp(math.Zero) <= 0 {
-		return false // require(dink > 0, "Dog/null-auction");
+func (s *dogBarkService) createTransaction(ctx context.Context, tx *types.Transaction) error {
+	err, txId := s.store.CreateTransaction(ctx, tx)
+
+	if err != nil {
+		log.Println("error in saving the transaction.", err)
+		return err
 	}
 
-	return true
+	receipt, err := s.waitForReceipt(ctx, tx.Hash())
+
+	if err != nil {
+		log.Println("error in waiting for receipt.", err)
+		return err
+	}
+
+	tLog := types.Log{BlockNumber: receipt.BlockNumber.Uint64()}
+	eLog := eth.Log{Log: tLog}
+
+	err = s.store.UpdateTransactionBlock(context.Background(), txId, receipt, uint64(eLog.Timestamp.Unix()), *receipt.BlockNumber, receipt.BlockHash)
+	if err != nil {
+		log.Println("error in updating the transaction receipt.", err)
+		return err
+	}
+
+	return nil
+}
+
+func (s *dogBarkService) newOsmAndPoke(ctx context.Context, pip common.Address) error {
+
+	osm, err := osm.NewOsm(pip, s.eth)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	opts, err := s.keeper.GetOpts()
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	tx, err := osm.Poke(opts)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	err = s.createTransaction(ctx, tx)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *dogBarkService) spotterPoke(ctx context.Context, ilk [32]byte) error {
+	opts, err := s.keeper.GetOpts()
+
+	if err != nil {
+		log.Fatal(err)
+		return nil
+	}
+
+	tx, err := s.spot.Poke(opts, ilk)
+
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	err = s.createTransaction(ctx, tx)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
